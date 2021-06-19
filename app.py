a20ee26023523a2cec98b35740af83bfb82d0af8
skmrSharma <54986978+skmrSharma@users.noreply.github.com>
2021-06-20 01:51:01 +0530

    implemented all buttons except tweets

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from flask.templating import render_template
 import subprocess
 # pandas not used here ?
 # import pandas as pd
 import csv
+from sentiment import sentimentCount
+from hash_src import sourceCount,hashtagCount
 
 app=Flask(__name__)
 
@@ -19,6 +21,22 @@ def tweet():
         cmd=' python "./twitter_search.py " {}'.format(post)
         p=subprocess.run(cmd,shell=True)
     return render_template('home.html',request_method=request_method)
+
+@app.route('/senti-button', methods=['GET','POST'])
+def giveSentiData():
+	pos,neg,neut = sentimentCount()
+	# return  '{} {} {}'.format(pos, neg, neut)
+	return jsonify(["Sentiment","Count"],["Positive", pos],["Negative", neg],["Neutral", neut])
+
+@app.route('/source-button', methods=['GET','POST'])
+def giveSourceData():
+	src = sourceCount()
+	return jsonify([["Source","Count"]] + src)
+
+@app.route('/hashtag-button', methods=['GET','POST'])
+def giveHashtagData():
+	topThreeHashtag = hashtagCount()[0:3]
+	return jsonify(topThreeHashtag)
 
 if __name__=="__main__":
     app.run(debug=True)
--- hash_src.py
+++ hash_src.py
@@ -1,17 +1,26 @@
 import pandas as pd
+import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
 
 df=pd.read_csv('result.csv')
 
-hash_values = df['hashtag'].value_counts().iloc[1:4].keys().tolist()
-hash_counts = df['hashtag'].value_counts().iloc[1:4].tolist()
+def hashtagCount():
+	hash_values = df['hashtag'].value_counts().keys().tolist()
+	# hash_counts = df['hashtag'].value_counts().tolist()
 
-for i in range(len(hash_values)):
-    print(hash_values[i],hash_counts[i])
+	# for i in range(len(hash_values)):
+	#	print(hash_values[i],hash_counts[i])
+	if "None" in hash_values:
+		hash_values.remove("None")
+	return hash_values
 
-src_values = df['source'].value_counts().keys().tolist()
-src_counts = df['source'].value_counts().tolist()
+def sourceCount():
+	src_values = df['source'].value_counts().keys().tolist()
+	src_counts = df['source'].value_counts().tolist()
 
-for i in range(len(src_values)):
-    print(src_values[i],src_counts[i])
+	# for i in range(len(src_values)):
+	#     print(src_values[i],src_counts[i])
+	# src = np.vstack((src_values,src_counts)).T
+	src = pd.DataFrame([src_values,src_counts]).T
+	return src.values.tolist()
